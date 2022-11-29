86026e440278b69ba4efefc7875bab01ff6ae229
Pedro Henrique <pedrohss466@gmail.com>
2022-11-28 23:54:22 -0300

    Desenvolvimento no Js

--- js/script.js
+++ js/script.js
@@ -3,46 +3,63 @@ var btn = document.querySelector('button.generate_btn')
 function born () {
     var year = Number(document.querySelector('input.year_born').value)
     var sex = document.getElementsByName('sex')
-    var resp = document.querySelector('p.text_down')
+    var resp = document.querySelector('div.text_down')
     var gen = ''
     var date = new Date()
     var date = date.getFullYear()
+    var img = document.querySelector('img.img_ger')
     subt = date-year
 
     console.log(year)
-    console.log(date)
     console.log(subt)
 
-    /*
     if (sex[0].checked) {
-        gen = 'Homem'
-        resp.innerText = `Um ${gen} de ${subt} anos`
-        console.log('Homem')
+        gen = 'Um Homem'
+        if (subt < 12) {
+            gen = 'Uma Criança'
+            img.setAttribute('src', 'download.jfif')
 
+        } else if (subt < 19) {
+            gen = 'Um Adolescente'
+            img.setAttribute('src', 'adolescente m.jpg')
+
+        } else if (subt < 55) {
+            gen = 'Um Adulto'
+            img.setAttribute('src', 'adulto m.jfif')
+
+        } else {
+            gen = 'Um Ancião'
+            img.setAttribute('src', 'aciao m.jpg')
+
+        }
+    
     } else if (sex[1].checked) {
-        gen = 'Mulher'
-        resp.innerText = `Um ${gen} de ${subt} anos`
-        console.log('Mulher')
+        gen = 'Uma Mulher'
+        if (subt < 12) {
+            gen = 'Uma Criança'
+            img.setAttribute('src', 'download.jfif')
+
+        } else if (subt < 19) {
+            gen = 'Uma Adolescente'
+            img.setAttribute('src', 'adolescente f.jfif')
+    
+        } else if (subt < 55) {
+            gen = 'Uma Adulta'
+            img.setAttribute('src', 'adulto f.jpg')
+
+        } else {
+            gen = 'Uma Anciã'
+            img.setAttribute('src', 'ancia f.jpg')
+    
+        }
 
     } else {
         console.log('Sexo não Definido')
 
     }
-    */
 
-    switch (sex) {
-        case sex[0] == sex[0].checked:
-            console.log('homem')
-            break
-        case sex[1] == sex[1].checked:
-            console.log('Mulher')
-            break
-        default:
-            console.log('Gênero Indefinido')
-            break
-    }
-
-    
+    resp.innerHTML = `${gen} de ${subt} anos`
+    img.appendChild(img)
 }
 
 btn.addEventListener("click", born)
